aab36ed67a0ab6d3a7f1b71db040c1a221a3b23f
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 11:17:35 -0500

    Add two actions in users controller - create and list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,26 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def list
+    @users = User.all
+  end
+
+  private
+    
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
