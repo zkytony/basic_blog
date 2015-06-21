04b15919885a37104c06c38c26da4ae887bf774a
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 18:33:57 -0500

    Migration for blog table

--- db/migrate/20150621225430_create_blogs.rb
+++ db/migrate/20150621225430_create_blogs.rb
@@ -7,5 +7,8 @@ class CreateBlogs < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    # Because we expect to retrieve all the blogs by a given user_id in reverse
+    # order of creation, add an index on user_id and created_at columns
+    add_index :blogs, [:user_id, :created_at]
   end
 end
