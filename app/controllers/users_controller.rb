1ef26f2c30852e7276d51102524eb9d4f9cb62ca
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 12:16:23 -0500

    Able to have a page for each user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save
+      # redirect to "users/:id"
+      flash[:success] = "Welcome to Bloggoor"
+      redirect_to @user
     else
+      # Render the form again; 
+      # Error will be displayed accordingly
       render :new
     end
   end
