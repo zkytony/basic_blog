763210573833af9f254587055ec5f32b51a0629d
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 17:43:10 -0500

    Include sessions helper in application controller. Session help contains log in log out and methods of this kind

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+
+  # Log in the given user
+  def log_in(user)
+    # session is a method defined by Rails
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    # is current_user nil?
+    !current_user.nil?
+  end
+
 end
