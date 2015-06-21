2a868800fbf7479c387bba77a765eaf0abda5bdc
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 15:44:28 -0500

    Routes for sessions configed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   get 'users/list' => 'users#list'
   resources :users, only: [:index, :new, :create, :show]
 
