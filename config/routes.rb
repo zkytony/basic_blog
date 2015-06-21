e0d11db313072d217a7e8ea826107d2ca7902914
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 00:31:16 -0500

    Routes routes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  root 'home#index'
-  get '/blog/create', to: 'blog#create', as: 'blog_create'
+  resources :users, only: [:index, :new, :show]
+
+  root 'static_pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
