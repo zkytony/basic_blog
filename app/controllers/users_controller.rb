1f402d7a317fce12146703f00802edbc9f79ba34
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 00:39:13 -0500

    Pretty stupid. Validation is for models! not for controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,5 @@
 class UsersController < ApplicationController
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true,
-                    uniqueness: { case_sensitive: false},
-                    format: { with: VALID_EMAIL_REGEX },
-                    length: { maximum: 50 }
-
-  validates :password, presence: true,
-                       length: { minimum: 8 }
-
-
   def index
 
   end
