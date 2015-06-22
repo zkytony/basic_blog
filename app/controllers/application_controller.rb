c6571cecb8e320deec1346e14f0bf3d13ace3311
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 20:07:51 -0500

    Add a private method logged_in_user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+    # confirms a logged-in user
+    def logged_in_user
+      if !logged_in?
+        flash[:danger] = "Log in first"
+        redirect_to login_url
+      end
+    end
 end
