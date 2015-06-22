e52a82faf6b44484961a0cf94f8c6484f36fdb9a
Kaiyu Zheng <kaiyutony@gmail.com>
2015-06-21 20:09:37 -0500

    Add validation for Blog model; Add associaiton between User and Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,8 @@
 class Blog < ActiveRecord::Base
+  belongs_to :user
+  # enforce the descending order by created_at of blogs
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :title,   presence:true,
+                      length: { maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :blogs, dependent: :destroy
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,    presence: true, 
                        uniqueness: { case_sensitive: false },
