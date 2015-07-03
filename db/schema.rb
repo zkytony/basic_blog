7272acb357c2b061f153d3e1f3cfb72a37c9c567
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-03 00:47:09 -0500

    New schema for added columns in users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621225430) do
+ActiveRecord::Schema.define(version: 20150703040434) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "user_id",    limit: 255
@@ -28,8 +28,11 @@ ActiveRecord::Schema.define(version: 20150621225430) do
     t.string   "password",   limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "name",       limit: 255
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", using: :btree
 
 end
